b7307f02d9e3a3489a66622d9d0ca47085d52a44
radhika inguva <radhika.inguva@gmail.com>
2022-06-14 21:44:36 -0700

    this is the first commit for this example-radhika

---
+++ src/test/java/com/jenkinsdemo/springexample/SpringJenkinsApplicationTests.java
@@ -0,0 +1,24 @@
+package com.jenkinsdemo.springexample;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	static Logger logger= LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+	@Test
+	void contextLoads() {
+		logger.info("test case executing..");
+		assertEquals(true, true);
+	}
+
+}
